87debf2959e7e44cd768a004ce3142efc1c1edb8
Chris <34993144+Mesiow@users.noreply.github.com>
2021-08-19 22:20:48 -0400

    add misc stores/loads decoding

--- BlissGBA/Cpu/Opcodes.cpp
+++ BlissGBA/Cpu/Opcodes.cpp
@@ -7,7 +7,7 @@ void Arm::mapArmOpcodes()
 {
 	for (s32 i = 0; i < 4096; i++) {
 		//SWI
-		if (i & 0xF00 == 0xF00) {
+		if ((i & 0xF00) == 0xF00) {
 			armlut[i] = b(&Arm::opSWI);
 		}
 		//Branch
@@ -21,6 +21,16 @@ void Arm::mapArmOpcodes()
 		else if (i == 0b000100100001) {
 			armlut[i] = b(&Arm::opBX);
 		}
+		//Addressing Mode 3 Misc Loads and Stores
+		else if (((i & 0b1001) == 0b1001) && ((i >> 9) == 0) && (((i >> 6) & 0x1) == 0x1)) {
+			armlut[i] = b(&Arm::executeMiscLoadStoreImm);
+		}
+		else if (((i & 0b1001) == 0b1001) && ((i >> 9) == 0) && (((i >> 6) & 0x1) == 0)) {
+			armlut[i] = b(&Arm::executeMiscLoadStoreReg);
+		}
+		else if ((((i >> 7) & 0x1F) == 0b00110) && ((i >> 4) & 0x3) == 0b00){
+			armlut[i] = b(&Arm::handleUndefinedIns);
+		}
 		//Addressing Mode 1 immediate with flags(S bit)
 		else if (((i >> 9) == 0b001) && ((i >> 4) & 0x1) == 1) {
 			armlut[i] = b(&Arm::executeDataProcessingImmFlags);
@@ -30,7 +40,7 @@ void Arm::mapArmOpcodes()
 			armlut[i] = b(&Arm::executeDataProcessingImm);
 		}
 		//Addressing Mode 1 immediate shift with flags(S bit)
-		else if (((i >> 9) == 0) && ((i & 0x1) == 0) && ((i >> 4) & 0x1 == 0x1)) {
+		else if (((i >> 9) == 0) && ((i & 0x1) == 0) && ((i >> 4) & 0x1) == 0x1) {
 			armlut[i] = b(&Arm::executeDataProcessingImmShiftFlags);
 		}
 		//Addressing Mode 1 immediate shift
@@ -43,6 +53,9 @@ void Arm::mapArmOpcodes()
 		}
 		else if ((i >> 9) == 0) {
 			armlut[i] = b(&Arm::executeDataProcessingRegShift);
+		}
+		else {
+			armlut[i] = b(&Arm::handleUndefinedIns);
 		}
 		
 	}
