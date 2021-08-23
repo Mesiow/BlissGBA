44cfb8481bc55841408065e0008b154f31efd973
Chris <34993144+Mesiow@users.noreply.github.com>
2021-08-22 22:01:14 -0400

    add components(cpu, ppu, debug)

--- BlissGBA/Core/Emulator.cpp
+++ BlissGBA/Core/Emulator.cpp
@@ -1,16 +1,34 @@
 #include "Emulator.h"
 
+Emulator::Emulator(sf::RenderWindow *window)
+	:mbus(), ppu(), cpu(&mbus), debug(window, this)
+{
+	showDebugger = true;
+	running = false;
+	debug.running = &running;
+}
+
 void Emulator::run()
 {
 	if (running) {
 		s32 cycles_this_frame = 0;
 		while (cycles_this_frame < maxCycles) {
-			//u8 cycle = cpu.clock();
-			//cycle *= 4;
+			u8 cycle = cpu.clock();
+			cycle *= 4;
 			//emulate timers
-			//ppu.update(cycles_this_frame);
+			ppu.update(cycles_this_frame);
 			//emulate interrupts
 		}
-		//update ppu framebuffer
+		ppu.bufferPixels();
 	}
+}
+
+void Emulator::render()
+{
+	if(showDebugger) debug.render();
+}
+
+void Emulator::reset()
+{
+
 }
--- BlissGBA/Core/Emulator.h
+++ BlissGBA/Core/Emulator.h
@@ -1,10 +1,20 @@
 #pragma once
-#include "../Utils.h"
-
+#include "../Debugger/DebugUI.h"
+#include "../Ppu/Ppu.h"
 
 class Emulator {
+public:
+	Emulator(sf::RenderWindow *window);
 	void run();
+	void render();
+	void reset();
 
+	MemoryBus mbus;
+	Ppu ppu;
+	Arm cpu;
+	DebugUI debug;
+
+	bool showDebugger; //if false, emulator will render full screen
 	bool running;
 	const int scanlinesPerFrame = 228;
 	const int maxCycles = (1232 * scanlinesPerFrame); //1232 cycles per scanline (308 dots * 4 cpu cycles)
