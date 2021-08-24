4b9b7fe46300b1e8d5b2d748edb7bd1eac67f85f
Chris <34993144+Mesiow@users.noreply.github.com>
2021-08-23 22:12:51 -0400

    implement bitmap mode 3(sorta)

--- BlissGBA/Ppu/Ppu.cpp
+++ BlissGBA/Ppu/Ppu.cpp
@@ -1,16 +1,80 @@
 #include "Ppu.h"
+#include "../Memory/MemoryBus.h"
+
+Ppu::Ppu(MemoryBus *mbus)
+	:mbus(mbus)
+{
+	mode3.pixels.create(SCREEN_WIDTH, SCREEN_HEIGHT, sf::Color::Black);
+	mode3.framebuffer.loadFromImage(mode3.pixels);
+	mode3.frame = sf::Sprite(mode3.framebuffer);
+
+	float scaleFactor = 2;
+	mode3.frame.setScale(scaleFactor, scaleFactor);
+}
 
 void Ppu::update(s32 cycles)
 {
+	scanlineCounter += cycles;
 
+	u16 lcd_control = readU16(LCD_CONTROL);
+	setBGMode(lcd_control);
+
+	//Bitmap mode 3
+	if (mode == BGMode::THREE) {
+		for (s32 y = 0; y < SCREEN_HEIGHT; y++) {
+			for (s32 x = 0; x < SCREEN_WIDTH; x++) {
+				//2 bytes associated to each pixel(defining one of the 32768 colors)
+				u16 pixel = readU16(VRAM_START + ((y * SCREEN_WIDTH + x) * mode3.bpp));
+
+				u8 red = getU8Color((pixel & 0x1F));
+				u8 green = getU8Color((pixel >> 5) & 0x1F);
+				u8 blue = getU8Color((pixel >> 10) & 0x1F);
+
+				const sf::Color color = sf::Color(red, green, blue, 255);
+				mode3.pixels.setPixel(x, y, color);
+			}
+		}
+	}
 }
 
 void Ppu::render(sf::RenderTarget& target)
 {
-
+	target.draw(mode3.frame);
 }
 
 void Ppu::bufferPixels()
 {
+	mode3.framebuffer.loadFromImage(mode3.pixels);
+}
 
+void Ppu::setBGMode(u16 lcdControl)
+{
+	u8 bg_mode = lcdControl & 0x7;
+	switch (bg_mode) {
+		case 0x0: mode = BGMode::ZERO; break;
+		case 0x1: mode = BGMode::ONE; break;
+		case 0x2: mode = BGMode::TWO; break;
+
+		case 0x3: mode = BGMode::THREE; break;
+		case 0x4: mode = BGMode::FOUR; break;
+		case 0x5: mode = BGMode::FIVE; break;
+	}
+}
+
+u8 Ppu::getU8Color(u8 color)
+{
+	u8 new_color = color << 3;
+	new_color |= (new_color >> 2);
+
+	return new_color;
+}
+
+u8 Ppu::readU8(u32 address)
+{
+	return mbus->readU8(address);
+}
+
+u16 Ppu::readU16(u32 address)
+{
+	return mbus->readU16(address);
 }
--- BlissGBA/Ppu/Ppu.h
+++ BlissGBA/Ppu/Ppu.h
@@ -1,17 +1,55 @@
 #pragma once
+#include <SFML\Graphics.hpp>
 #include "../Utils.h"
 
-#define PPU_WIDTH 240
-#define PPU_HEIGHT 160
+class MemoryBus;
+
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 160
+
+#define VRAM_START 0x06000000
+#define BM_MODE3_SIZE 0x12BFF
+
+//LCD Registers
+#define LCD_CONTROL 0x04000000
+
+enum class BGMode {
+	//Tile/Map modes
+	ZERO = 0,
+	ONE,
+	TWO,
+
+	//Bitmap modes
+	THREE,
+	FOUR,
+	FIVE
+};
 
 struct BitmapMode3 {
 	static constexpr u8 bpp = 2; //2 bytes per pixel
-	u8 framebuffer[PPU_WIDTH * PPU_HEIGHT * bpp];
+	sf::Image pixels;
+	sf::Texture framebuffer;
+	sf::Sprite frame;
 };
 
 class Ppu {
+public:
+	Ppu(MemoryBus *mbus);
 	void update(s32 cycles);
+	void render(sf::RenderTarget& target);
 	void bufferPixels();
 
+	void setBGMode(u16 lcdControl);
+
+	//Extend 5 bit color val into 8 bits
+	u8 getU8Color(u8 color);
+
+	u8 readU8(u32 address);
+	u16 readU16(u32 address);
+
+	BGMode mode;
 	BitmapMode3 mode3;
+
+	u32 scanlineCounter = 0;
+	MemoryBus* mbus;
 };
